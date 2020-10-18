b8845e31b884e0392efb0fdd23b130aa5df58195
Frida BV <44652162+SuFrida@users.noreply.github.com>
2020-10-17 21:51:12 -0500

    Add files via upload
    Cambiamos manualmente la ubicación de nuestro código de front-end, ya que nos percatamos que dicho código se ubicaba en una rama y no en el main original.

---
+++ MiniHackInklu-master/src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyB5VmM0DiL8H3SBIzgsySqpr-OWQ1yh8aM",
+    authDomain: "inklu-database.firebaseapp.com",
+    databaseURL: "https://inklu-database.firebaseio.com",
+    projectId: "inklu-database",
+    storageBucket: "inklu-database.appspot.com",
+    messagingSenderId: "954105702909",
+    appId: "1:954105702909:web:d9cbc3959c5e0f8e005194",
+    measurementId: "G-K7EREP5T1J"
+}
